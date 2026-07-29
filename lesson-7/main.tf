2953eeb8986d53c125e753474c474325ef3381ae
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2026-07-29 15:07:50 +0200

    fixes

--- lesson-7/main.tf
+++ lesson-7/main.tf
@@ -12,26 +12,44 @@ provider "aws" {
   region = var.aws_region
 }
 
-# S3 backend
 module "s3_backend" {
   source      = "./modules/s3-backend"
   bucket_name = var.bucket_name
   table_name  = var.dynamodb_table_name
 }
 
-# Чистый вызов VPC (все настройки внутри модуля или через дефолтные переменные)
 module "vpc" {
   source = "./modules/vpc"
+
+  vpc_cidr_block = "10.0.0.0/16"
+
+  public_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24"
+  ]
+
+  private_subnets = [
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24"
+  ]
+
+  availability_zones = [
+    "us-west-2a",
+    "us-west-2b",
+    "us-west-2c"
+  ]
+
+  vpc_name = "lesson-7-vpc"
 }
 
-# Репозиторий ECR
 module "ecr" {
   source       = "./modules/ecr"
   ecr_name     = var.ecr_name
   scan_on_push = true
 }
 
-# Модуль EKS, который явно зависит от vpc и ecr
 module "eks" {
   source             = "./modules/eks"
   cluster_name       = "lesson-7-eks"
